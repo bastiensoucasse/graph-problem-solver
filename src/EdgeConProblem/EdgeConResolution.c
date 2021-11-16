21ace0dfda49584c44afdeb3fde4d3607c85cd11
Bastien Soucasse <bastien.soucasse@icloud.com>
2021-11-16 11:54:56 +0100

    BruteForceEdgeCon almost (missing loop)

--- src/EdgeConProblem/EdgeConResolution.c
+++ src/EdgeConProblem/EdgeConResolution.c
@@ -27,7 +27,7 @@ static int computeMaxDepth(int numNodes, int depth[])
     int max = -1;
     for (int i = 0; i < numNodes; i++)
     {
-        if(depth[i] > max)
+        if (depth[i] > max)
             max = depth[i];
     }
     return max;
@@ -49,26 +49,32 @@ static void browse(EdgeConGraph graph, int u, int depth[])
     int numNodes = orderG(graphInit);
     for (int v = 0; v < numNodes; v++)
     {
-        if (v != u && isEdge(graphInit, u, v) && depth[v] == - 1)
+        if (v != u && isEdge(graphInit, u, v) && depth[v] == -1)
         {
-            if (isTranslator(graph, u, v))
+            if (isTranslator(graph, u, v)) // should be heterogeneous
+            {
                 depth[v] = depth[u] + 1;
-            else 
+                browse(graph, v, depth);
+            }
+            else if (isEdgeHomogeneous(graph, u, v))
+            {
                 depth[v] = depth[u];
-            browse(graph, v, depth);
+                browse(graph, v, depth);
+            }
         }
     }
 }
 
 /**
- * @brief Brute Force Algorithm. If there is a result, the solution will be stored in @param graph, and its homogeneous components updated. If no solution, graph won't be modified. Returns the maximal cost of communication for any choice of translators.
+ * @brief Get the highest cost of communication between two nodes with a set of choosen transducers.
  * 
  * @param graph An instance of the problem.
- * @return the maximal cost that two nodes communicate with for any possible set of transducers of minimal size. Returns -1 if there is no solution (i.e., the graph is not connex).
- * 
+ * @param translators A set of transducers.
+ * @return the maximal cost that cost that two nodes communicate with for the set of transducers translators.
+ *
  * @pre graph must be valid.
  */
-int BruteForceEdgeCon(EdgeConGraph graph)
+static int getMaxPathLength(EdgeConGraph graph, int *translators)
 {
     int max = 0;
     Graph graphInit = getGraph(graph);
@@ -91,4 +97,72 @@ int BruteForceEdgeCon(EdgeConGraph graph)
     }
     printf("Result is : %d !\n", max);
     return max;
+}
+
+static int nbTranslators(EdgeConGraph graph)
+{
+    int numOfTranslators = 0;
+    int n = orderG(getGraph(graph));
+    for (int u = 0; u < n; u++)
+        for (int v = n - 1; v > u; v--)
+            numOfTranslators += isTranslator(graph, u, v);
+    return numOfTranslators;
+}
+
+static bool nextTranslatorSet(EdgeConGraph graph, int N, int n, int H_t, int **hE)
+{
+    while (nbTranslators(graph) != N)
+    {
+        for (int i = 0; i < H_t; i++)
+        {
+            int u = hE[0][i];
+            int v = hE[1][i];
+            if (!isTranslator(graph, u, v))
+            {
+                addTranslator(graph, u, v);
+                break;
+            }
+            else
+            {
+                removeTranslator(graph, u, v);
+                if (i == H_t - 1)
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+/**
+ * @brief Brute Force Algorithm. If there is a result, the solution will be stored in @param graph, and its homogeneous components updated. If no solution, graph won't be modified. Returns the maximal cost of communication for any choice of translators.
+ * 
+ * @param graph An instance of the problem.
+ * @return the maximal cost that two nodes communicate with for any possible set of transducers of minimal size. Returns -1 if there is no solution (i.e., the graph is not connex).
+ * 
+ * @pre graph must be valid.
+ */
+int BruteForceEdgeCon(EdgeConGraph graph)
+{
+    int n = orderG(getGraph(graph));           // Number of vertices
+    int H_t = getNumHeteregeneousEdges(graph); // Number of heterogeneous edges
+    int N = getNumComponents(graph) - 1;       // Number of homogeneous components
+    int *hE[2];                                // Heterogeneous edges saved
+    hE[0] = (int *)malloc(H_t * sizeof(int));
+    hE[1] = (int *)malloc(H_t * sizeof(int));
+
+    printf("n = %d, H_t = %d, N = %d\n", n, H_t, N);
+
+    int i = 0;
+    for (int u = 0; u < n; u++)
+        for (int v = n - 1; v > u; v--)
+            if (isEdgeHeterogeneous(graph, u, v))
+            {
+                hE[0][i] = u;
+                hE[1][i] = v;
+                i++;
+            }
+
+    nextTranslatorSet(graph, N, n, H_t, hE); // TODO: loop
+
+    printTranslator(graph);
 }
