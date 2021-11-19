e0bdb6dd713500de0785b9eef73fef1ec459f554
Bastien Soucasse <bastien.soucasse@icloud.com>
2021-11-19 21:25:19 +0100

    Clang formatting
    Co-authored-by: iprovost <iprovost@users.noreply.github.com>

--- src/EdgeConProblem/EdgeConReduction.c
+++ src/EdgeConProblem/EdgeConReduction.c
@@ -3,16 +3,20 @@
  * @author Quentin Desire
  * @author Iantsa Provost
  * @author Bastien Soucasse
- * @brief An implementation of the Coca project for 2021 year. Converts a 2-coloured graph g and an integer k to a formula true if and only if it exists a translator set of minimal size which forces two nodes to communicate with cost strictly bigger than k.
- * Provides functions to generate the formula and the necessary variables, alongside function to decode a solution from a model of the formula.
+ * @brief An implementation of the Coca project for 2021 year. Converts a
+ * 2-coloured graph g and an integer k to a formula true if and only if it
+ * exists a translator set of minimal size which forces two nodes to communicate
+ * with cost strictly bigger than k. Provides functions to generate the formula
+ * and the necessary variables, alongside function to decode a solution from a
+ * model of the formula.
  * @version 1
  * @date 2021-11-26
- * 
+ *
  * @copyright Creative Commons
- * 
+ *
  */
 #include "EdgeConReduction.h"
-#include "Z3Tools.h"
+
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,17 +24,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "Z3Tools.h"
+
 /**
- * @brief Creates a variable representing that the edge (@p node1, @p node2) has translator number @p number. The variable name will always start with the smaller node.
- * 
+ * @brief Creates a variable representing that the edge (@p node1, @p node2) has
+ * translator number @p number. The variable name will always start with the
+ * smaller node.
+ *
  * @param ctx The solver context
  * @param node1 A node
  * @param node2 A node
  * @param number A number
- * @return Z3_ast The lowest variable in lexicographic order among [(@p node1, @p node2),number] and [(@p node2, @p node1),number]
+ * @return The lowest variable in lexicographic order among [(@p node1,
+ * @p node2),number] and [(@p node2, @p node1),number]
  */
-Z3_ast getVariableIsIthTranslator(Z3_context ctx, int node1, int node2, int number)
-{
+Z3_ast getVariableIsIthTranslator(Z3_context ctx, int node1, int node2,
+                                  int number) {
     char name[40];
     if (node1 < node2)
         snprintf(name, 40, "[(%d,%d),%d]", node1, node2, number);
@@ -40,30 +49,32 @@ Z3_ast getVariableIsIthTranslator(Z3_context ctx, int node1, int node2, int numb
 }
 
 /**
- * @brief Get the Variable p_{@p child,@p parent} representing that the connected component @p parent is the parent of the connected component @p child in the reduction.
- * 
+ * @brief Get the Variable p_{@p child,@p parent} representing that the
+ * connected component @p parent is the parent of the connected component @p
+ * child in the reduction.
+ *
  * @param ctx The solver context.
  * @param child The number of a connected component.
  * @param parent The number of a connected component.
- * @return Z3_ast A formula representing p_{@p child,@p parent}.
+ * @return A formula representing p_{@p child,@p parent}.
  */
-Z3_ast getVariableParent(Z3_context ctx, int child, int parent)
-{
+Z3_ast getVariableParent(Z3_context ctx, int child, int parent) {
     char name[40];
     snprintf(name, 40, "p_[%d,%d]", child, parent);
     return mk_bool_var(ctx, name);
 }
 
 /**
- * @brief Get the Variable l_{@p component,@p level} representing that the connected component @p component is at level @p level in the spanning tree.
- * 
+ * @brief Get the Variable l_{@p component,@p level} representing that the
+ * connected component @p component is at level @p level in the spanning tree.
+ *
  * @param ctx The solver context.
  * @param level The level.
  * @param component The number of the component.
- * @return Z3_ast A formula representing l_{@p component,@p level}
+ * @return A formula representing l_{@p component,@p level}
  */
-Z3_ast getVariableLevelInSpanningTree(Z3_context ctx, int level, int component)
-{
+Z3_ast getVariableLevelInSpanningTree(Z3_context ctx, int level,
+                                      int component) {
     char name[40];
     snprintf(name, 40, "l_[%d,%d]", component, level);
     return mk_bool_var(ctx, name);
@@ -71,7 +82,7 @@ Z3_ast getVariableLevelInSpanningTree(Z3_context ctx, int level, int component)
 
 /**
  * @brief Makes the set of x_{e,i} from the graph.
- * 
+ *
  * @param ctx The solver context.
  * @param g The graph as an EdgeConGraph.
  * @param n The number of vertices.
@@ -79,19 +90,15 @@ Z3_ast getVariableLevelInSpanningTree(Z3_context ctx, int level, int component)
  * @param N The number of translators.
  * @return The set of x_{e,i} created.
  */
-Z3_ast **makeX(Z3_context ctx, EdgeConGraph g, int n, int H_t, int N)
-{
+Z3_ast **makeX(Z3_context ctx, EdgeConGraph g, int n, int H_t, int N) {
     Z3_ast **X = (Z3_ast **)malloc(H_t * sizeof(Z3_ast *));
-    for (int i = 0; i < H_t; i++)
-        X[i] = (Z3_ast *)malloc(N * sizeof(Z3_ast));
+    for (int i = 0; i < H_t; i++) X[i] = (Z3_ast *)malloc(N * sizeof(Z3_ast));
 
     int e = 0;
     for (int u = 0; u < n; u++)
         for (int v = n - 1; v > u; v--)
-            if (isEdgeHeterogeneous(g, u, v))
-            {
-                for (int i = 0; i < N; i++)
-                {
+            if (isEdgeHeterogeneous(g, u, v)) {
+                for (int i = 0; i < N; i++) {
                     X[e][i] = getVariableIsIthTranslator(ctx, u, v, i);
                     printf("Creating x_%d,%d (e = %d,%d)…\n", e, i, u, v);
                 }
@@ -102,30 +109,26 @@ Z3_ast **makeX(Z3_context ctx, EdgeConGraph g, int n, int H_t, int N)
 
 /**
  * @brief Frees the set of x_{e,i}.
- * 
+ *
  * @param X The set of x_{e,i}.
  * @param H_t The number of heterogenous edges.
  */
-void freeX(Z3_ast **X, int H_t)
-{
-    for (int i = 0; i < H_t; i++)
-        free(X[i]), X[i] = NULL;
+void freeX(Z3_ast **X, int H_t) {
+    for (int i = 0; i < H_t; i++) free(X[i]), X[i] = NULL;
     free(X), X = NULL;
 }
 
 /**
  * @brief Creates a formula for at least one literal set to TRUE.
- * 
+ *
  * @param ctx The solver context.
  * @param X The set of x_{e,i}.
  * @param size The size of the set of x_{e,i}.
  * @return The final formula.
  */
-Z3_ast atLeast(Z3_context ctx, Z3_ast *X, int size)
-{
+Z3_ast atLeast(Z3_context ctx, Z3_ast *X, int size) {
     Z3_ast formula = Z3_mk_false(ctx);
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         Z3_ast args[2] = {formula, X[i]};
         formula = Z3_mk_or(ctx, 2, args);
     }
@@ -134,18 +137,16 @@ Z3_ast atLeast(Z3_context ctx, Z3_ast *X, int size)
 
 /**
  * @brief Creates a formula for at most one literal set to TRUE.
- * 
+ *
  * @param ctx The solver context.
  * @param X The set of x_{e,i}.
  * @param size The size of the set of x_{e,i}.
  * @return The final formula.
  */
-Z3_ast atMost(Z3_context ctx, Z3_ast *X, int size)
-{
+Z3_ast atMost(Z3_context ctx, Z3_ast *X, int size) {
     Z3_ast formula = Z3_mk_true(ctx);
     for (int i = 0; i < size; i++)
-        for (int j = i + 1; j < size; j++)
-        {
+        for (int j = i + 1; j < size; j++) {
             Z3_ast args[2] = {Z3_mk_not(ctx, X[i]), Z3_mk_not(ctx, X[j])};
             Z3_ast clause = Z3_mk_or(ctx, 2, args);
 
@@ -157,34 +158,30 @@ Z3_ast atMost(Z3_context ctx, Z3_ast *X, int size)
 }
 
 /**
- * @brief Creates a formula that ensures that 
- * each translator can only be associated to exactly one edge,
- * and each edge can only receive at most one translator.
- 
+ * @brief Creates a formula that ensures that each translator can only be
+ associated to at most one edge, and each edge can only receive at most one
+ translator.
+
  * @param ctx The solver context.
  * @param X The set of x_{e,i}.
  * @param H_t The number of heterogeneous edges.
  * @param N The max number of translators.
- * @return Z3_ast The final formula.
+ * @return The final formula.
  */
-Z3_ast phi1(Z3_context ctx, Z3_ast **X, int H_t, int N)
-{
+Z3_ast phi1(Z3_context ctx, Z3_ast **X, int H_t, int N) {
     Z3_ast left = Z3_mk_true(ctx);
     Z3_ast right = Z3_mk_true(ctx);
 
     Z3_ast Xi[H_t];
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {
         // Retrieve every x_e,i with i settled (the column i)
-        for (int e = 0; e < H_t; e++)
-            Xi[e] = X[e][i];
+        for (int e = 0; e < H_t; e++) Xi[e] = X[e][i];
 
         Z3_ast argsLeft[2] = {left, atMost(ctx, Xi, H_t)};
         left = Z3_mk_and(ctx, 2, argsLeft);
     }
 
-    for (int e = 0; e < H_t; e++)
-    {
+    for (int e = 0; e < H_t; e++) {
         Z3_ast argsRight[2] = {right, atMost(ctx, X[e], N)};
         right = Z3_mk_and(ctx, 2, argsRight);
     }
@@ -194,27 +191,29 @@ Z3_ast phi1(Z3_context ctx, Z3_ast **X, int H_t, int N)
 }
 
 /**
- * @brief Generates a SAT formula satisfiable if and only if there is a set of translators of cost @p cost such that the graph admits a valid path between any two nodes.
- * 
+ * @brief Generates a SAT formula satisfiable if and only if there is a set of
+ * translators of cost @p cost such that the graph admits a valid path between
+ * any two nodes.
+ *
  * @param ctx The solver context.
  * @param graph A EdgeConGraph.
  * @param cost The cost of the translator set.
- * @return Z3_ast The formula.
- * @pre graph must be an initialized EdgeConGraph with computed connected components.
+ * @return The formula.
+ * @pre graph must be an initialized EdgeConGraph with computed connected
+ * components.
  */
-Z3_ast EdgeConReduction(Z3_context ctx, EdgeConGraph edgeGraph, int cost)
-{
+Z3_ast EdgeConReduction(Z3_context ctx, EdgeConGraph graph, int cost) {
     // Z3_ast X[5];
     // for (int i = 0; i < 5; i++)
     //     X[i] = Z3_mk_true(ctx);
     // return atMost(ctx, X, 5);
 
-    int n = orderG(getGraph(edgeGraph));
-    int H_t = getNumHeteregeneousEdges(edgeGraph);
-    int N = getNumComponents(edgeGraph) - 1;
+    int n = orderG(getGraph(graph));
+    int H_t = getNumHeteregeneousEdges(graph);
+    int N = getNumComponents(graph) - 1;
     printf("n = %d, H_t = %d, N = %d\n", n, H_t, N);
 
-    Z3_ast **X = makeX(ctx, edgeGraph, n, H_t, N);
+    Z3_ast **X = makeX(ctx, graph, n, H_t, N);
 
     // TODO…
     Z3_ast test = phi1(ctx, X, H_t, N);
@@ -225,16 +224,19 @@ Z3_ast EdgeConReduction(Z3_context ctx, EdgeConGraph edgeGraph, int cost)
 }
 
 /**
- * @brief Gets the translator set from a model and adds it to the EdgeConGraph. It also computes the homogeneous components taking into account the translators (this number should be one).
- * 
+ * @brief Gets the translator set from a model and adds it to the EdgeConGraph.
+ * It also computes the homogeneous components taking into account the
+ * translators (this number should be one).
+ *
  * @param ctx The solver context.
  * @param model A variable assignment.
  * @param graph A EdgeConGraph.
- * 
+ *
  * @pre @p model must be a valid model.
- * @pre @p graph must be a valid EdgeConGraph with no translators (or at least the original number of homogeneous components).
+ * @pre @p graph must be a valid EdgeConGraph with no translators (or at least
+ * the original number of homogeneous components).
  */
-void getTranslatorSetFromModel(Z3_context ctx, Z3_model model, EdgeConGraph graph)
-{
+void getTranslatorSetFromModel(Z3_context ctx, Z3_model model,
+                               EdgeConGraph graph) {
     return;
 }
--- src/EdgeConProblem/EdgeConResolution.c
+++ src/EdgeConProblem/EdgeConResolution.c
@@ -6,57 +6,51 @@
  * @brief  Algorithms to solve directly the EdgeCon problem
  * @version 1
  * @date 2021-11-26
- * 
+ *
  * @copyright Creative Commons.
- * 
+ *
  */
 #include "EdgeConResolution.h"
-#include "Graph.h"
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Graph.h"
+
 /**
  * @brief Compare each depth, stock and then return the highest value.
- * 
+ *
  * @param numNodes Number of nodes in the EdgeConGraph.
  * @param depth An array filled with depth of each nodes.
  * @return the maximum of depth array.
  */
-static int computeMaxDepth(int numNodes, int depth[])
-{
+int computeMaxDepth(int numNodes, int *depth) {
     int max = -1;
-    for (int i = 0; i < numNodes; i++)
-    {
-        if (depth[i] > max)
-            max = depth[i];
+    for (int i = 0; i < numNodes; i++) {
+        if (depth[i] > max) max = depth[i];
     }
     return max;
 }
 
 /**
- * @brief Browse neighbours of node u given and register the depth of each nodes (one depth per homogeneous components).
- * Recursively loop on all the graph.
- * 
+ * @brief Browse neighbours of node u given and register the depth of each nodes
+ * (one depth per homogeneous components). Recursively loop on all the graph.
+ *
  * @param graph An instance of the problem.
- * @param u Node u that we will explore each neighbours (if it hasn't been explored yet).
+ * @param u Node u that we will explore each neighbours (if it hasn't been
+ * explored yet).
  * @param depth An array filled with depth of each nodes.
  */
-static void browse(EdgeConGraph graph, int u, int depth[])
-{
+void browse(EdgeConGraph graph, int u, int *depth) {
     Graph graphInit = getGraph(graph);
     int numNodes = orderG(graphInit);
-    for (int v = 0; v < numNodes; v++)
-    {
-        if (v != u && isEdge(graphInit, u, v) && depth[v] == -1)
-        {
-            if (isTranslator(graph, u, v)) // should be heterogeneous
-            {
+    for (int v = 0; v < numNodes; v++) {
+        if (v != u && isEdge(graphInit, u, v) && depth[v] == -1) {
+            if (isTranslator(graph, u, v)) {  // Should be heterogeneous
                 depth[v] = depth[u] + 1;
                 browse(graph, v, depth);
-            }
-            else if (isEdgeHomogeneous(graph, u, v))
-            {
+            } else if (isEdgeHomogeneous(graph, u, v)) {
                 depth[v] = depth[u];
                 browse(graph, v, depth);
             }
@@ -65,28 +59,25 @@ static void browse(EdgeConGraph graph, int u, int depth[])
 }
 
 /**
- * @brief Get the highest cost of communication between two nodes with a set of choosen transducers.
- * 
+ * @brief Get the highest cost of communication between two nodes with a set of
+ * choosen transducers.
+ *
  * @param graph An instance of the problem.
- * @return the maximal cost that cost that two nodes communicate with for the set of transducers translators.
+ * @return the maximal cost that cost that two nodes communicate with for the
+ * set of transducers translators.
  */
-static int getMaxPathLength(EdgeConGraph graph)
-{
+int getMaxPathLength(EdgeConGraph graph) {
     int max = 0;
     Graph graphInit = getGraph(graph);
     int numNodes = orderG(graphInit);
     int depth[numNodes];
 
-    for (int i = 0; i < numNodes; i++)
-    {
-        for (int j = 0; j < numNodes; j++)
-            depth[j] = -1;
+    for (int i = 0; i < numNodes; i++) {
+        for (int j = 0; j < numNodes; j++) depth[j] = -1;
         depth[i] = 0;
         browse(graph, i, depth);
-        for (int k = 0; k < numNodes; k++)
-        {
-            if (depth[k] == -1)
-                return 0;
+        for (int k = 0; k < numNodes; k++) {
+            if (depth[k] == -1) return 0;
             int maxDepth = computeMaxDepth(numNodes, depth);
             max = maxDepth > max ? maxDepth : max;
         }
@@ -96,23 +87,22 @@ static int getMaxPathLength(EdgeConGraph graph)
 
 /**
  * @brief Computes the total number of translators in the graph.
- * 
+ *
  * @param graph An instance of the problem.
  * @return The number of translators in @param graph.
  */
-static int nbTranslators(EdgeConGraph graph)
-{
-    int numOfTranslators = 0;
+int nbTranslators(EdgeConGraph graph) {
+    int numTranslators = 0;
     int n = orderG(getGraph(graph));
     for (int u = 0; u < n; u++)
         for (int v = n - 1; v > u; v--)
-            numOfTranslators += isTranslator(graph, u, v);
-    return numOfTranslators;
+            numTranslators += isTranslator(graph, u, v);
+    return numTranslators;
 }
 
 /**
  * @brief Apply the next set of translators of size N in the graph.
- * 
+ *
  * @param graph An instance of the problem.
  * @param N The number of translators we need to have.
  * @param n The number of vertices in the graph.
@@ -120,28 +110,21 @@ static int nbTranslators(EdgeConGraph graph)
  * @param hE The custom heteregeneous edges matrix.
  * @return true if a next translator set has been found, false otherwise.
  */
-static bool nextTranslatorSet(EdgeConGraph graph, int N, int n, int H_t, int **hE)
-{
-    do
-    {
+bool nextTranslatorSet(EdgeConGraph graph, int N, int n, int H_t, int **hE) {
+    do {
         // printf("-------\n");
-        for (int i = 0; i < H_t; i++)
-        {
+        for (int i = 0; i < H_t; i++) {
             int u = hE[0][i];
             int v = hE[1][i];
             // printf("Index #%d: %d-%d\n", i, u, v);
-            if (!isTranslator(graph, u, v))
-            {
+            if (!isTranslator(graph, u, v)) {
                 // printf("addTranslator\n");
                 addTranslator(graph, u, v);
                 break;
-            }
-            else
-            {
+            } else {
                 // printf("removeTranslator\n");
                 removeTranslator(graph, u, v);
-                if (i == H_t - 1)
-                    return false;
+                if (i == H_t - 1) return false;
             }
         }
     } while (nbTranslators(graph) != N);
@@ -150,14 +133,13 @@ static bool nextTranslatorSet(EdgeConGraph graph, int N, int n, int H_t, int **h
 
 /**
  * @brief Creates the custom heterogeneous edges matrix.
- * 
+ *
  * @param graph An instance of the problem.
  * @param n The number of vertices.
  * @param H_t The number of heterogeneous edges.
  * @return a pointer on the created matrix.
  */
-static int **createHE(EdgeConGraph graph, int n, int H_t)
-{
+int **createHE(EdgeConGraph graph, int n, int H_t) {
     int **hE = (int **)malloc(2 * sizeof(int *));
     hE[0] = (int *)malloc(H_t * sizeof(int));
     hE[1] = (int *)malloc(H_t * sizeof(int));
@@ -165,49 +147,48 @@ static int **createHE(EdgeConGraph graph, int n, int H_t)
     int i = 0;
     for (int u = 0; u < n; u++)
         for (int v = n - 1; v > u; v--)
-            if (isEdgeHeterogeneous(graph, u, v))
-            {
+            if (isEdgeHeterogeneous(graph, u, v)) {
                 hE[0][i] = u;
                 hE[1][i] = v;
                 i++;
             }
-    
+
     return hE;
 }
 
 /**
  * @brief Frees the heterogeneous edges matrix.
- * 
+ *
  * @param hE The heterogeneous edges matrix.
  */
-void freeHE(int **hE)
-{
-    for (int i = 0; i < 2; i++)
-        free(hE[i]), hE[i] = NULL;
+void freeHE(int **hE) {
+    for (int i = 0; i < 2; i++) free(hE[i]), hE[i] = NULL;
     free(hE), hE = NULL;
 }
 
 /**
- * @brief Brute Force Algorithm. If there is a result, the solution will be stored in @param graph, and its homogeneous components updated. If no solution, graph won't be modified. Returns the maximal cost of communication for any choice of translators.
- * 
+ * @brief Brute Force Algorithm. If there is a result, the solution will be
+ * stored in @param graph, and its homogeneous components updated. If no
+ * solution, graph won't be modified. Returns the maximal cost of communication
+ * for any choice of translators.
+ *
  * @param graph An instance of the problem.
- * @return the maximal cost that two nodes communicate with for any possible set of transducers of minimal size. Returns -1 if there is no solution (i.e., the graph is not connex).
- * 
+ * @return the maximal cost that two nodes communicate with for any possible set
+ * of transducers of minimal size. Returns -1 if there is no solution (i.e., the
+ * graph is not connex).
+ *
  * @pre graph must be valid.
  */
-int BruteForceEdgeCon(EdgeConGraph graph)
-{
-    int n = orderG(getGraph(graph));           // Number of vertices
-    int H_t = getNumHeteregeneousEdges(graph); // Number of heterogeneous edges
-    int N = getNumComponents(graph) - 1;       // Number of homogeneous components
-    int **hE = createHE(graph, n, H_t);        // Heterogeneous edges custom
+int BruteForceEdgeCon(EdgeConGraph graph) {
+    int n = orderG(getGraph(graph));            // Number of vertices
+    int H_t = getNumHeteregeneousEdges(graph);  // Number of heterogeneous edges
+    int N = getNumComponents(graph) - 1;  // Number of homogeneous components
+    int **hE = createHE(graph, n, H_t);   // Heterogeneous edges custom
 
     int min = INT_MAX;
-    while (nextTranslatorSet(graph, N, n, H_t, hE))
-    {
+    while (nextTranslatorSet(graph, N, n, H_t, hE)) {
         int k = getMaxPathLength(graph);
-        if (k < min && k != 0)
-            min = k;
+        if (k < min && k != 0) min = k;
     }
     resetTranslator(graph);
 
